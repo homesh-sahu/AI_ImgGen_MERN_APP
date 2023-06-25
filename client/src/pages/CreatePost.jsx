b8fd9bff10378d7b2c00f4303126f08228da0359
Homesh <harshsahu9926@gmail.com>
2023-06-25 17:14:48 +0530

    Version 2.0

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -21,7 +21,7 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true);
         const response = await fetch(
-          "https://ai-imagegen-app.onrender.com/api/v1/dalle", //Calling our api from backend
+          "https://http://localhost:8080/api/v1/dalle", //Calling our api from backend
           {
             method: "POST",
             headers: {
@@ -50,7 +50,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("https://ai-imagegen-app.onrender.com/api/v1/post", {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
